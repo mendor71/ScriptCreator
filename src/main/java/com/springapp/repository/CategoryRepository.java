766c563a8ce583210057fe9988a5d09bb85e0aa3
mendor71 <mendor71@gmail.com>
2018-05-17 16:07:38 +0500

    refactoring

--- src/main/java/com/springapp/repository/CategoryRepository.java
+++ src/main/java/com/springapp/repository/CategoryRepository.java
@@ -3,9 +3,12 @@ package com.springapp.repository;
 import com.springapp.entity.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 /**
  * Created by Mendor on 25.12.2017.
  */
 public interface CategoryRepository extends CrudRepository<Category, Long> {
     Category findByCatName(String catName);
+    List<Category> findByCatStateIdStateName(String stateName);
 }
